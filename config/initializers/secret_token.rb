eddd8f2a495a1a69bf6319f99208f38e79be6a4b
vagrant <vagrant@precise32.(none)>
2013-02-09 22:37:10 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFirstRoRApp::Application.config.secret_token = 'babc5ac96f17acd07d9249f50edb752591ca78474459b326a8b8691c25ac5571d6a23bd692beef2446c742d6581c7bc8879d43f39651307ef40c9b6879cbbe42'
